e25eac5495d8669d3e6b03e40859501d10bf0666
Davide Ginnasio <d.ginnasio@tdnet.it>
2021-12-01 14:52:08 +0100

    refactor

--- src/com/company/day1/Day1.java
+++ src/com/company/day1/Day1.java
@@ -6,31 +6,29 @@ import static com.company.utility.Util.readFile;
 
 public class Day1 {
     public static void main(String[] args) {
-        var list = readFile("src/com/company/day1/d1input.txt");
-        var increased = 0;
-        increased = p1(list, 0);
-        System.out.println(increased);
-        increased = p2(list, 0);
-        System.out.println(increased);
+        p1(readFile("src/com/company/day1/d1input.txt"));
+        p2(readFile("src/com/company/day1/d1input.txt"));
     }
 
-    private static int p1(List<Integer> list, int increased) {
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i - 1) - list.get(i) < 0) {
-                increased++;
+    private static void p1(List<Integer> l) {
+        var c = 0;
+        for (int i = 1; i < l.size(); i++) {
+            if (l.get(i - 1) - l.get(i) < 0) {
+                c++;
             }
         }
-        return increased;
+        System.out.println(c);
     }
 
-    private static int p2(List<Integer> list, int increased) {
-        for (int i = 3; i < list.size(); i++) {
-            if ((list.get(i - 3) + list.get(i - 2) + list.get(i - 1)) -
-                    (list.get(i - 2) + list.get(i - 1) + list.get(i)) < 0) {
-                increased++;
+    private static void p2(List<Integer> l) {
+        var c = 0;
+        for (int i = 3; i < l.size(); i++) {
+            if ((l.get(i - 3) + l.get(i - 2) + l.get(i - 1)) -
+                    (l.get(i - 2) + l.get(i - 1) + l.get(i)) < 0) {
+                c++;
             }
         }
-        return increased;
+        System.out.println(c);
     }
 
 }

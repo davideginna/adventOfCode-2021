a21d9e7384463de15e3301232b41c58de103bba5
Davide Ginnasio <d.ginnasio@tdnet.it>
2021-12-01 12:47:49 +0100

    refactor

--- src/com/company/day1/Day1.java
+++ src/com/company/day1/Day1.java
@@ -6,8 +6,8 @@ public class Day1 {
     public static void main(String[] args) {
         var list = readFile("src/com/company/day1/d1input.txt");
         var increased = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (i != 0 && list.get(i - 1) - list.get(i) < 0) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i - 1) - list.get(i) < 0) {
                 increased++;
             }
         }

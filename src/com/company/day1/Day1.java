1990589e97e5da051a4a64d5e90a68cbe535fbe5
Davide Ginnasio <d.ginnasio@tdnet.it>
2021-12-01 12:32:55 +0100

    gitignore

--- src/com/company/day1/Day1.java
+++ src/com/company/day1/Day1.java
@@ -7,29 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Day1 {
-
     public static void main(String[] args) {
         var list = readFile();
         var increased = 0;
-        var decreased = 0;
-        var c = 0;
         for (int i = 0; i < list.size(); i++) {
-            if (i == 0) {
-                c = list.get(0);
-            } else {
-                //0:149
-                //1:163
-                if (c - list.get(i) < 0) {
-                    increased++;
-                } else {
-                    decreased++;
-                }
-                c = list.get(i);
+            if (i != 0 && list.get(i - 1) - list.get(i) < 0) {
+                increased++;
             }
-
         }
         System.out.println(increased);
-
     }
 
 

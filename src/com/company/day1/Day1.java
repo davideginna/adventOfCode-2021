014b04fc4ccec71db54a126715a16cc1dda78b58
Davide Ginnasio <d.ginnasio@tdnet.it>
2021-12-01 12:44:27 +0100

    refactor

--- src/com/company/day1/Day1.java
+++ src/com/company/day1/Day1.java
@@ -1,14 +1,10 @@
 package com.company.day1;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import static com.company.utility.Util.readFile;
 
 public class Day1 {
     public static void main(String[] args) {
-        var list = readFile();
+        var list = readFile("src/com/company/day1/d1input.txt");
         var increased = 0;
         for (int i = 0; i < list.size(); i++) {
             if (i != 0 && list.get(i - 1) - list.get(i) < 0) {
@@ -16,20 +12,5 @@ public class Day1 {
             }
         }
         System.out.println(increased);
-    }
-
-
-    private static List<Integer> readFile() {
-        List<Integer> integers = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("src/com/company/day1/d1input.txt"))) {
-            String line = br.readLine();
-            while (line != null) {
-                integers.add(Integer.parseInt(line));
-                line = br.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return integers;
     }
 }
--- src/com/company/utility/Util.java
+++ src/com/company/utility/Util.java
@@ -1,8 +1,24 @@
 package com.company.utility;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Util {
-    public Util() {
+
+    public static List<Integer> readFile(String filename) {
+        List<Integer> integers = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line = br.readLine();
+            while (line != null) {
+                integers.add(Integer.parseInt(line));
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return integers;
     }
-
-
 }
